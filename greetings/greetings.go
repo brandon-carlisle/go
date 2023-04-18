6d2591e036137744d0ac2baf8d3dbbea2c72d4fb
Brandon <93879641+beanziii@users.noreply.github.com>
2023-04-18 22:45:47 +0100

    error

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,12 +1,19 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person
 
-func Hello(name string) string {
-	// Return a greeting that embeds name into message
+func Hello(name string) (string, error) {
+	// if no name given return error with message
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 
+	// if name, return a greeting that embeds name into message
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
